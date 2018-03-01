f3cf1fe2925f4346e8b5888f28c11f80b38cefd6
unStatiK <unstatik@staremax.com>
2018-03-01 06:06:17 +0300

    reformat

--- src/main/groovy/com/bingo/Main.groovy
+++ src/main/groovy/com/bingo/Main.groovy
@@ -5,13 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.ApplicationContext
 import org.springframework.scheduling.annotation.EnableScheduling
 
-import java.time.LocalDateTime
-
 @SpringBootApplication
 @EnableScheduling
 class Main {
-    static void main(String[] args) {	    
-		assert args.size() != 0 : "Should pass config path!"
+    static void main(String[] args) {
+        assert args.size() != 0: "Should pass config path!"
         ApplicationContext context = SpringApplication.run(Main.class, args)
         BingoBot bingoBot = context.getBean(BingoBot.class)
         bingoBot.init(args[0])  // pass config path
